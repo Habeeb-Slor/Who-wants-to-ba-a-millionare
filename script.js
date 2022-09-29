8a2850c2aa828e7edf1ff2651fa53bc4f487138e
Oshoala Habeebullah <oshoalahabeebullah@gmail.com>
2022-09-29 13:01:03 +0100

    fixing questions

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let questionBank = [
   {
-      question : 'Entomology is the science that studies',
-      option : ['Behavior of human beings','Insects','The origin and history of technical and scientific terms','The formation of rocks'],
-      answer : 'Insects'
+      question : 'In the UK, the abbreviation NHS stands for National what Service?',
+      option : ['Humanity','Health','Honour','Household'],
+      answer : 'Health'
   },
   {
       question : 'Eritrea, which became the 182nd member of the UN in 1993, is in the continent of',
       option : ['Asia','Africa','Europe','Australia'],
       answer : 'Africa'
-  },
-  {
+    },
+    {
       question : 'For which of the following disciplines is Nobel Prize awarded?',
       option : ['Physics and Chemistry','All of the above','Literature, Peace and Economics','Physiology or Medicine'],
       answer : 'All of the above'
@@ -27,12 +27,12 @@ let questionBank = [
   {
       question: 'Which of these teams never won the Uefa Champions League',
       option : ['Blackburn','Bayern Munich','Manchester City','Liverpool'],
-      answer : 'Germany'
+      answer : 'Manchester City'
   },
   {
-      question: "The 'Black flag' signifies",
-      option : ['revolution/danger','protest','peace','truce'],
-      answer : 'protest'
+      question: "The first person to walk on the moon is",
+      option : ['Neil Armstrong','Hilary Clinton','Nelson Mandela','Oshoala Habibullah'],
+      answer : 'Neil Armstrong'
   },
   {
       question: 'ICAO stands for',
@@ -40,10 +40,10 @@ let questionBank = [
       answer : 'International Civil Aviation Organization'
   },
   {
-      question: 'The number of already named bones in the human skeleton is',
-      option : ['200','206','212','218'],
-      answer : '206'
-  },
+      question: 'What is 3(2-1)',
+      option : ['1','2','3','4'],
+      answer : '3'
+    },
   {
       question: 'When light passes from air into glass it experiences change of',
       option : ['frequency and wavelength','wavelength and speed','frequency and speed','frequency, wavelength and speed'],
@@ -80,38 +80,55 @@ function calcScore(e){
       score += 5000;
       document.getElementById(e.id).style.background = 'limegreen';
       rightSong.play()
-  }
+      
+    }
+  
   else{
-      wrongSong.play()
-      document.getElementById(e.id).style.background = 'tomato';
-     setTimeout(() => {window.location.href = "gameover.html"} ,1700)
+    wrongSong.play()
+    document.getElementById(e.id).style.background = 'tomato';
+      setTimeout(() => {window.location.href = "gameover.html"} ,1700)
+    }
+    setTimeout(nextQuestion,2000);
   }
-  setTimeout(nextQuestion,2000);
-}
-var countDown =()=>{
+  var countDown =()=>{
   let countedSecs = secs.value;
   countedSecs-- ;
   secs.value =  countedSecs
   myTimeout = setTimeout(countDown, 1000)
   if(countedSecs==0){
-      gameOver.innerHTML = "Time Up"
-      gameOver.style.color = "red"
-      secs.value = "00"
-      secs.style.color = "red"
+    gameOver.innerHTML = "Time Up"
+    gameOver.style.color = "red"
+    secs.value = "00"
+    secs.style.color = "red"
       clearInterval(myTimeout)
       setTimeout(() => {
           location.reload()
-      }, 1150);
-  }
+        }, 1150);
+      }
 }
 function nextQuestion(){
+  
   if(i < questionBank.length - 1){
+    document.getElementById('option_1').style.background = 'blueviolet';
+    document.getElementById('option_2').style.background = 'blueviolet';
+    document.getElementById('option_3').style.background = 'blueviolet';
+    document.getElementById('option_4').style.background = 'blueviolet';
+    
       i += 1
       displayQuestion();
-  }
-  else{
-      points.innerHTML =`Congratulations you have earned $ ${score}`;
-      quizContainer.style.display = 'none'
-      scoreboard.style.display = "block"
-  }
+      
+    }
+    else{
+      question_body.style.display = 'none';
+      won.style.display = 'block';
+      won.innerHTML = `
+      <p class="fs-1 congracts">Congractulatios</p>
+      <p class="fs-2 text-light">You are one of the lucky people to have been able to win a sum of $ ${score}</p>
+      <button class="btn btn-outline-success" onclick="reStart()">Start Again</button>
+
+      `
+    }
+}
+function reStart(){
+  location.reload()
 }
