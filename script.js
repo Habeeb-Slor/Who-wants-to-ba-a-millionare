37f32ab659f7d11865a030549854039b0ed05885
Oshoala Habeebullah <oshoalahabeebullah@gmail.com>
2022-09-29 04:46:11 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,67 @@
+let questionBank = [
+  {
+      question : 'Entomology is the science that studies',
+      option : ['Behavior of human beings','Insects','The origin and history of technical and scientific terms','The formation of rocks'],
+      answer : 'Insects'
+  },
+  {
+      question : 'Eritrea, which became the 182nd member of the UN in 1993, is in the continent of',
+      option : ['Asia','Africa','Europe','Australia'],
+      answer : 'Africa'
+  },
+  {
+      question : 'For which of the following disciplines is Nobel Prize awarded?',
+      option : ['Physics and Chemistry','All of the above','Literature, Peace and Economics','Physiology or Medicine'],
+      answer : 'All of the above'
+  },
+  {
+      question : 'Exposure to sunlight helps a person improve his health because',
+      option : ['the infrared light kills bacteria in the body','the ultraviolet rays convert skin oil into Vitamin D','resistance power increases','the pigment cells in the skin get stimulated and produce a healthy tan'],
+      answer : 'the ultraviolet rays convert skin oil into Vitamin D'
+  },
+  {
+      question: 'Philology is the',
+      option : ['study of bones','science of languages','study of muscles','study of architecture'],
+      answer : 'science of languages'
+  },
+  {
+      question: 'Which of these teams never won the Uefa Champions League',
+      option : ['Blackburn','Bayern Munich','Manchester City','Liverpool'],
+      answer : 'Germany'
+  },
+  {
+      question: "The 'Black flag' signifies",
+      option : ['revolution/danger','protest','peace','truce'],
+      answer : 'protest'
+  },
+  {
+      question: 'ICAO stands for',
+      option : ['Indian Corporation of Agriculture Organization','International Civil Aviation Organization','Institute of Company of Accounts Organization','None of the above'],
+      answer : 'International Civil Aviation Organization'
+  },
+  {
+      question: 'The number of already named bones in the human skeleton is',
+      option : ['200','206','212','218'],
+      answer : '206'
+  },
+  {
+      question: 'When light passes from air into glass it experiences change of',
+      option : ['frequency and wavelength','wavelength and speed','frequency and speed','frequency, wavelength and speed'],
+      answer : 'wavelength and speed'
+  }
+]
+
+let i = 0;
+let mySong = new Audio('Audio/playing.mp3')
+
+
+
+function displayQuestion(){
+  question.innerHTML = questionBank[i].question;
+  option_1.innerHTML = questionBank[i].option[0];
+  option_2.innerHTML = questionBank[i].option[1];
+  option_3.innerHTML = questionBank[i].option[2];
+  option_4.innerHTML = questionBank[i].option[3];
+  mySong.play()
+}
+displayQuestion()
